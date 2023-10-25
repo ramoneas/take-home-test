ac071b8726fafcd62228f9c3877e558464bf2cf5
RamonAlvarez1 <ramonemmanuelalvarezsantana2@gmail.com>
2023-10-25 18:23:18 -0400

    Build Swagger documentation and main.ts config

--- tht-api/src/main.ts
+++ tht-api/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Take-home-test')
+    .setDescription('API for Take Home Test')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+
+  await app.listen(port);
 }
 bootstrap();
